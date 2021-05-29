b1a5a3472a4e7dfef3e608fee77a50085413a8cd
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-05-29 12:01:59 -0300

    fix: :ambulance: fixing the StackNavigation reset utility

--- src/tools/ResetNavigation.ts
+++ src/tools/ResetNavigation.ts
@@ -1,10 +1,10 @@
 import { CommonActions } from '@react-navigation/native';
 
-export const resetStackNavigation = (navigation: any) => {
+export const resetStackNavigation = (navigation: any, routeName: string) => {
   navigation.dispatch(
     CommonActions.reset({
-      index: 0,
-      routes: [{ name: 'TabNavigatorRoutes' }]
+      index: 1,
+      routes: [{ name: routeName }]
     })
   );
 }

7a85465204e006e4d182cc09a55b60feed627466
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-05-30 09:05:55 -0300

    fix: :art: correcting the payment date formatting

--- src/utils/DateFormats.ts
+++ src/utils/DateFormats.ts
@@ -1,17 +1,28 @@
 import moment from 'moment';
+import 'moment/locale/pt-br';
 
-export const inDDdays = (timestamp: number | undefined) => {
-  if (timestamp !== undefined) {
-    const momentUnix = moment().unix() * 1000;
-    const timestampUnix = moment(timestamp * 1000);
-    const diffInDays = timestampUnix.diff(momentUnix, 'days');
+moment.locale('pt-br');
 
-    if (diffInDays <= 0) {
-      return `Recebido!`
-    } else {
-      return `em ${diffInDays} dias`;
-    }
-  } else {
-    return '';
-  }
-}
+export const inDDdays = (date: string) => {
+  const now = moment().unix() * 1000;
+
+  const toString = date.toString();
+  const day = toString.slice(0, 2);
+  const month = toString.slice(3, 5);
+  const year = toString.slice(6, 8);
+
+  const newFormat = `${month}/${day}/${year}`;
+
+  const convertFormat = moment(new Date(newFormat), 'DD/MM/YYYY');
+
+  const diffInDays = convertFormat.diff(now, 'days');
+  const diffInHours = convertFormat.diff(now, 'hours');
+
+  if (diffInDays <= 0 && diffInHours <= 0) return 'Recebido!';
+
+  if (diffInDays === 0 && diffInHours >= 0) return 'Amanhã';
+
+  if (diffInDays > 0 && diffInHours > 0) return `em ${diffInDays} dias`;
+
+  return '';
+};

d0c37e76853292bcb54c3a79bb0e00fb7784413e
Lucas Cavalcante <cavalcantelucasx@gmail.com>
2021-05-30 15:43:31 -0300

    refactor: :zap: external font loading

--- App.tsx
+++ App.tsx
@@ -1,15 +1,35 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import {
+  useFonts,
+  IBMPlexSans_400Regular,
+  IBMPlexSans_500Medium,
+  IBMPlexSans_600SemiBold,
+} from '@expo-google-fonts/ibm-plex-sans';
 
 import { AssetProvider } from '~/context/AssetContext';
 import { SettingProvider } from '~/context/SettingContext';
+import { NotificationProvider } from '~/context/NotificationContext';
 import Routes from '~/routes/app.routes';
 
-const App: React.FC = () => (
-  <SettingProvider>
-    <AssetProvider>
-      <Routes />
-    </AssetProvider>
-  </SettingProvider>
-);
+const App: React.FC = () => {
+  const [fontLoaded] = useFonts({
+    IBMPlexSans_400Regular,
+    IBMPlexSans_500Medium,
+    IBMPlexSans_600SemiBold,
+  });
+
+  if (!fontLoaded) return null;
+
+  return (
+    <NotificationProvider>
+      <SettingProvider>
+        <AssetProvider>
+          <Routes />
+        </AssetProvider>
+      </SettingProvider>
+    </NotificationProvider>
+  );
+};
 
 export default App;
